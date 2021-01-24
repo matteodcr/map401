368366dca4e81765383c160b81d7cbfc1f5bc516
Mattéo Decorsaire <mattdecorsaire@gmail.com>
2021-01-24 20:18:21 +0100

    correction geom2d test

--- test_geom2d.c
+++ test_geom2d.c
@@ -17,28 +17,28 @@ int main(int argc, char *argv[]) {
     Point D = set_point(atoi(argv[7]), atoi(argv[8]));
     
     printf("=======\n");
-    printf("Point A: (%f, %f)\n", A.x, A.y);
-    printf("Point B: (%f, %f)\n", B.x, B.y);
-    printf("Point C: (%f, %f)\n", C.x, C.y);
-    printf("Point D: (%f, %f)\n", D.x, D.y);
+    printf("Point A: (%.0f, %.0f)\n", A.x, A.y);
+    printf("Point B: (%.0f, %.0f)\n", B.x, B.y);
+    printf("Point C: (%.0f, %.0f)\n", C.x, C.y);
+    printf("Point D: (%.0f, %.0f)\n", D.x, D.y);
     printf("=======\n");
 
     //Calculs
 
     
-    printf("Addition des points A et B : (%f, %f)\n", add_point(A, B).x, add_point(A, B).y);
-    printf("Distance entre A et B : %d\n", dist_points(A, B));
+    printf("Addition des points A et B : (%.0f, %.0f)\n", add_point(A, B).x, add_point(A, B).y);
+    printf("Distance entre A et B : %f\n", dist_points(A, B));
     printf("======\n");
 
     Vecteur AB = vect_bipoint(A, B);
     Vecteur CD = vect_bipoint(C, D);
-    printf("Vecteur AB : (%f, %f)\n", AB.x, AB.y);
-    printf("Vecteur CD : (%f, %f)\n", CD.x, CD.y);
-    printf("Somme des vecteurs AB et CD : (%f, %f)\n", somme_vect(AB, CD).x, somme_vect(AB, CD).y);
-    printf("Produit de AB par 3 : (%f, %f)\n", produit_reel_vect(5, AB).x, produit_reel_vect(5, AB).y);
-    printf("Produit de AB par C : (%f, %f)\n", produit_point_vect(C, AB).x, produit_point_vect(C, AB).y);
+    printf("Vecteur AB : (%.0f, %.0f)\n", AB.x, AB.y);
+    printf("Vecteur CD : (%.0f, %.0f)\n", CD.x, CD.y);
+    printf("Somme des vecteurs AB et CD : (%.0f, %.0f)\n", somme_vect(AB, CD).x, somme_vect(AB, CD).y);
+    printf("Produit de AB par 3 : (%.0f, %.0f)\n", produit_reel_vect(3, AB).x, produit_reel_vect(3, AB).y);
+    printf("Produit de AB par C : (%.0f, %.0f)\n", produit_point_vect(C, AB).x, produit_point_vect(C, AB).y);
     printf("Produit scalaire de AB et CD : %d\n", produit_scalaire(AB, CD));
-    printf("Norme du vecteur AB : %d\n", norme_vect(AB));
+    printf("Norme du vecteur AB : %f\n", norme_vect(AB));
     
     
     return 0;
