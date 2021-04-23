c5b97539e8cd8c78f81b3a3f545794b7bdc9b529
Wartapoo <grangemathis@gmail.com>
2021-04-23 16:34:14 +0200

    Added demo_temps.sh

---
+++ demo_temps.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo Test sur $1
+echo ==================
+echo Sans simplification
+\time -f "%U" ./test_eps Images/pbm/$1.pbm Images/resultat-script/[CONTOUR]$1.eps
+
+echo ==================
+echo Simplification segments
+
+\time -f "Seuil 1 : %U" ./test_douglas_peucker_bezier1 Images/pbm/$1.pbm Images/resultat-script/[BEZIER1][1]$1.eps 1
+\time -f "Seuil 3 : %U" ./test_douglas_peucker_bezier1 Images/pbm/$1.pbm Images/resultat-script/[BEZIER1][3]$1.eps 3
+\time -f "Seuil 10 : %U" ./test_douglas_peucker_bezier1 Images/pbm/$1.pbm Images/resultat-script/[BEZIER1][10]$1.eps 10
+
+echo ==================
+echo Simplification Bézier de degré 2
+
+\time -f "Seuil 1 : %U" ./test_douglas_peucker_bezier2 Images/pbm/$1.pbm Images/resultat-script/[BEZIER2][1]$1.eps 1
+\time -f "Seuil 3 : %U" ./test_douglas_peucker_bezier2 Images/pbm/$1.pbm Images/resultat-script/[BEZIER2][3]$1.eps 3
+\time -f "Seuil 10 : %U" ./test_douglas_peucker_bezier2 Images/pbm/$1.pbm Images/resultat-script/[BEZIER2][10]$1.eps 10
+
+echo ==================
+echo Simplification Bézier de degré 3
+
+\time -f "Seuil 1 : %U" ./test_douglas_peucker_bezier3 Images/pbm/$1.pbm Images/resultat-script/[BEZIER3][1]$1.eps 1
+\time -f "Seuil 3 : %U" ./test_douglas_peucker_bezier3 Images/pbm/$1.pbm Images/resultat-script/[BEZIER3][3]$1.eps 3
+\time -f "Seuil 10 : %U" ./test_douglas_peucker_bezier3 Images/pbm/$1.pbm Images/resultat-script/[BEZIER3][10]$1.eps 10
